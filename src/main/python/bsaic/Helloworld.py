654e193b82c0e2b786affa47634dd6697e60e244
Atchaya T <atchaya.602@gmail.com>
2025-08-15 18:14:21 +0530

    code committed

--- src/main/python/bsaic/Helloworld.py
+++ src/main/python/bsaic/Helloworld.py
@@ -2,5 +2,11 @@ print("Hello World")
 
 ## from branch code changes
 print("Welcome to Devops")
+
 ## from d1 branch code changes
 print("All is well")
+
+
+## from c1 branch
+print("Good in Devops")
+
